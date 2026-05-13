dcac61f9e8c173ecde628d4f9a4865249d1f038a
picotfr46400-cmyk <picotfr46400@gmail.com>
2026-05-13 16:10:35 +0200

    Delete picotrack-main directory

--- picotrack-main/app.js
+++
@@ -1,8 +0,0 @@
-/*
-  PicoTrack est maintenant découpé dans /js.
-  Ce fichier est conservé volontairement presque vide pour éviter les confusions.
-  Les scripts sont chargés directement dans index.html, dans cet ordre :
-  1. js/core/*
-  2. js/features/*
-  3. js/init.js
-*/
