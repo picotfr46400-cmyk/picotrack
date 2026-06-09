3ee8097a0edc34a39203fb157eed9c0465b8959f
picotfr46400-cmyk <picotfr46400@gmail.com>
2026-06-09 11:03:14 +0200

    Delete js directory

--- js/core/state.js
+++
@@ -1,9 +0,0 @@
-// ══ ÉTAT ══
-let curForm=null,filtered=[...FORMS_DATA],sortCol='nom',sortDir=1;
-let pageSize=10,curPage=1;
-let builderFields=[],formColor='#3b82f6',formModules=['general'];
-let layoutRows=[],declItems=[],curFieldIdx=null,bTab='gen';
-let previewValues={},previewMode='sup';
-let cfgOpen=false,cfgTab='G';
-let saisieValues={},curSaisieFormId=null;
-
