a4628d0a8e9de6650d56f6fe7dcbd68d7aaac680
picotfr46400-cmyk <picotfr46400@gmail.com>
2026-06-10 15:31:25 +0200

    Delete api directory

--- api/runtime-config.js
+++
@@ -1,1 +0,0 @@
-module.exports=function handler(req,res){res.statusCode=410;res.setHeader('Content-Type','application/json; charset=utf-8');res.setHeader('Cache-Control','no-store');res.end(JSON.stringify({error:'runtime-config désactivé. Utiliser /api/bootstrap.'}));};
