8413b2c568b1aff8d34d7ae953de4df6bd045fc3
picotfr46400-cmyk <picotfr46400@gmail.com>
2026-06-17 14:33:55 +0200

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE='picotrack-v33-domain-config-v21';
+const CACHE='picotrack-v34-env-license-v22';
 self.addEventListener('install',event=>self.skipWaiting());
 self.addEventListener('activate',event=>event.waitUntil(
   caches.keys().then(keys=>Promise.all(keys.map(key=>caches.delete(key)))).then(()=>self.clients.claim())
