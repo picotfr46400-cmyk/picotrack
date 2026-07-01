9790470cb7475020bdb3eb1dcf67fa36418a9e1a
picotfr46400-cmyk <picotfr46400@gmail.com>
2026-07-01 17:56:29 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='picotrack-v48-client-global-split-v28';
+const CACHE='picotrack-v49-client-global-split-v28';
 self.addEventListener('install',event=>self.skipWaiting());
 self.addEventListener('activate',event=>event.waitUntil(
   caches.keys().then(keys=>Promise.all(keys.map(key=>caches.delete(key)))).then(()=>self.clients.claim())
